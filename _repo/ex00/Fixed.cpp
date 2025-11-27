23af8046496c401977a666ce2aa3f6e55b48119d
Samuel D'Hooree <samd-hoo@a1-r3-p11.s19.be>
2025-11-27 13:29:51 +0100

    main : update name

--- _repo/ex00/Fixed.cpp
+++ _repo/ex00/Fixed.cpp
@@ -1,8 +1,8 @@
 #include "Fixed.hpp"
 
-const int Fixed::number_of_fractional_bits = 8;
+const int Fixed::numberOfFractionalBits = 8;
 
-Fixed::Fixed() : fixed_point_number_value(0)
+Fixed::Fixed() : fixedPointNumberValue(0)
 {
 	std::cout << "Default constructor called" << std::endl;
 }
@@ -10,13 +10,13 @@ Fixed::Fixed() : fixed_point_number_value(0)
 Fixed::Fixed(const Fixed &other)
 {
 	std::cout << "Copy constructor called" << std::endl;
-	this->fixed_point_number_value = other.fixed_point_number_value;
+	this->fixedPointNumberValue = other.fixedPointNumberValue;
 }
 
 Fixed	&Fixed::operator=(const Fixed &other)
 {
 	std::cout << "Copy assignment operator" << std::endl;
-	this->fixed_point_number_value = other.fixed_point_number_value;
+	this->fixedPointNumberValue = other.fixedPointNumberValue;
 	return (*this);
 }
 
@@ -27,41 +27,10 @@ Fixed::~Fixed()
 
 int	Fixed::getRawBits(void) const
 {
-	return (this->fixed_point_number_value);
+	return (this->fixedPointNumberValue);
 }
 
 void	Fixed::setRawBits(int const raw)
 {
-	this->fixed_point_number_value = raw;
+	this->fixedPointNumberValue = raw;
 }
-
-#define P(element) \
-	std::cout  << #element << " = (" << element << ")"<< std::endl;
-
-void Fixed::Test()
-{
-	// P(fixed_point_number_value);
-	// P(number_of_fractional_bits);
-}
-
-
-
-/*
- $> ./a.out
-Default constructor called
-Copy constructor called
-Copy assignment operator called // <-- This line may be missing depending on your implementation
-getRawBits member function called
-Default constructor called
-Copy assignment operator called
-getRawBits member function called
-getRawBits member function called
-0
-getRawBits member function called
-0
-getRawBits member function called
-0
-Destructor called
-Destructor called
-Destructor called
-*/
--- _repo/ex00/Fixed.hpp
+++ _repo/ex00/Fixed.hpp
@@ -4,8 +4,8 @@
 class	Fixed
 {
 	private:
-		int			fixed_point_number_value;
-		static const int	number_of_fractional_bits;
+		int			fixedPointNumberValue;
+		static const int	numberOfFractionalBits;
 
 	public:
 		Fixed();
@@ -19,21 +19,3 @@ class	Fixed
 
 
 };
-
-
-/*
- *
-Private members:
-◦ An integer to store the fixed-point number value.
-◦ A static constant integer to store the number of fractional bits. Its value
-will always be the integer literal 8.
-• Public members:
-◦ A default constructor that initializes the fixed-point number value to 0.
-◦ A copy constructor.
-◦ A copy assignment operator overload.
-◦ A destructor.
-◦ A member function int getRawBits( void ) const;
-that returns the raw value of the fixed-point value.
-◦ A member function void setRawBits( int const raw );
-that sets the raw value of the fixed-point number.
-*/
